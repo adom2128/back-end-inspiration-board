72432cfeb7ffd8e7ddfcbb363f948684cf386d92
Alejandra <adom2128@gmail.com>
2023-06-26 14:55:59 -0600

    adds from_dict classmethod and post endpoint to Board

--- app/models/board.py
+++ app/models/board.py
@@ -1,7 +1,19 @@
 from app import db
+from flask import abort, make_response, jsonify
 
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
     card = db.relationship("Card", back_populates="board")
+
+    @classmethod
+    def from_dict(cls, board_data):
+        try:
+            new_board = Board(title=board_data["title"],
+                              owner=board_data["owner"],
+                              )
+        except KeyError:
+            abort(make_response(jsonify({"details": "Invalid data"}), 400))
+
+        return new_board
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,5 +1,15 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
-# example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+@board_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    new_board = Board.from_dict(request_body)
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return make_response(jsonify({"board": new_board.to_dict()}), 201)
