dbe51e1418f16b767ea8aef0f273b19aefc6f416
Angie Tran <tranangiekim@gmail.com>
2023-06-26 11:54:28 -0600

    added card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,9 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String(40), nullable=False)
+    likes_count = db.Column(db.Integer)
+    color = db.Column(db.String, default=0)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
+    board = db.relationship("Board", back_populates = "card")
