d09388c5567b6bb84ff258e5f6c415a9b6dfa395
Cindy Vides <civides1@gmail.com>
2023-06-26 11:06:58 -0700

    importing models

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 
+
 db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
@@ -19,6 +20,8 @@ def create_app():
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
+    from app.models.board import Board
+    from app.models.card import Card
 
     db.init_app(app)
     migrate.init_app(app, db)
