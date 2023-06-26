23508a16e65dcf3396a7b5da2d338a392091fcf4
Danica <danicas.edu@gmail.com>
2023-06-26 13:56:13 -0700

    adds GET method to board_routes and adds to_dict helper function

--- app/models/board.py
+++ app/models/board.py
@@ -5,3 +5,13 @@ class Board(db.Model):
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
     card = db.relationship("Card", back_populates="board")
+
+
+
+def to_dict(self):
+        board_as_dict = {}
+        board_as_dict["id"]=self.board_id
+        board_as_dict["title"]=self.title
+        board_as_dict["owner"]=self.owner
+
+        return board_as_dict 
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,5 +1,19 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+@board_bp.route("", methods=["GET"])
+def read_all_boards():
+    
+    boards = Board.query.all()
+
+    board_response = []
+    
+    for board in boards:
+        board_response.append(board.to_dict())
+
+    return jsonify(board_response), 200
