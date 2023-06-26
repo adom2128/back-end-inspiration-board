0c8b2b37dfa93d30e68e84ba54d32785f6664bc6
Danica <danicas.edu@gmail.com>
2023-06-26 11:13:23 -0700

    adds card to Board model

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,4 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    card = db.relationship("Card", back_populates = "board")
