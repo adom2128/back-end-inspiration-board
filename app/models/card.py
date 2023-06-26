9d8f40651ce6b7c8bce90a248614b8bd65143587
Danica <danicas.edu@gmail.com>
2023-06-26 11:22:49 -0700

    finishes setup for Card and Board models

--- app/models/card.py
+++ app/models/card.py
@@ -1,9 +1,9 @@
 from app import db
 
 class Card(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String(40), nullable=False)
     likes_count = db.Column(db.Integer)
     color = db.Column(db.String, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"))
-    board = db.relationship("Board", back_populates = "card")
+    board = db.relationship("Board", back_populates="card")
